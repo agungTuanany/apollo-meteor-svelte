cdc2774b8ad726a817ea10c376683d5828e4769d
agungTuanany <agung.tuanany@gmail.com>
2020-07-24 19:46:43 +0700

    Create Apollo schema

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,4 +1,26 @@
 import { createApolloServer } from "meteor/apollo"
 import { makeExecutableSchema } from "graphql-tools"
 
-createApolloServer({})
+// GrahpQL Schema
+const typeDefs = `
+    type Query {
+        hello: String
+    }
+`;
+
+// Actual Method for server-side
+const resolvers = {
+    // Media method wrapper
+    Query: {
+        hello() {
+            return "Hello Level Up"
+        }
+    }
+}
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+createApolloServer({ schema })
